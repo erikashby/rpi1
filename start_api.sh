db2ecb699ea7a1b2afc316b829c5931bb2c2ce1f
Erik Ashby <erik@ashbys.net>
2023-12-22 06:00:10 +0000

    Updating startup sequence

--- start_api.sh
+++ start_api.sh
@@ -1,10 +1,10 @@
 #Script to update and start the API service through flask
 #This script will run automatically through a cronjob on boot
 
-#Part 1 - Update API app folder
-cd /home/erika/app/rpi1/
-sleep 30
-git pull https://github.com/erikashby/rpi1
+#Part 1 - Update API app folder -- Not working
+#cd /home/erika/app/rpi1/
+#sleep 30
+#git pull https://github.com/erikashby/rpi1
 
 #Part 2 - Start flask
 #   activate the virtual enviornment
