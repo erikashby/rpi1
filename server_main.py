305eeeab789976772fe9a0568b66de19d9ad4f9b
kodemeex <115670027+kodemeex@users.noreply.github.com>
2023-12-27 20:18:08 +0000

    get action w

--- server_main.py
+++ server_main.py
@@ -10,10 +10,17 @@ def hello_world():
 @app.route('/testput', methods=["PUT"])
 def test_put():
     get_ip = '1'
+    get_action = 'toggle'
     event = request.json
     for i in event:
         if i == 'source ip':
             get_ip = event[i]
+        if i == 'action':
+            get_action = event[i]
     
     print("source ip = " + get_ip)
+
+    send_toggle = "http://" + get_ip + ":5000/node/light?id=led0&action=" + get_action
+
     return "<h1>TEST<h1>"
+
