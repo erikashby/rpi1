7f8761efa222524087320bdda0b592e38b016798
kodemeex <115670027+kodemeex@users.noreply.github.com>
2023-12-27 21:17:04 +0000

    toggle light

--- server_main.py
+++ server_main.py
@@ -10,7 +10,8 @@ def hello_world():
 @app.route('/testput', methods=["PUT"])
 def test_put():
     get_ip = '1'
-    get_action = 'on'
+    get_state = '0'
+    action = 'on'
     event = request.json
     for i in event:
         if i == 'source ip':
@@ -19,9 +20,16 @@ def test_put():
     status_url = "http://" + get_ip + ":5000/node/status"
     status = requests.get(status_url).json()
 
-    get_action = status['status']['light_status'][0]['state']
-    print(get_action)
-    send_toggle = "http://" + get_ip + ":5000/node/light?id=led0&action="
+    get_state = status['status']['light_status'][0]['state']
+
+    if get_state == '0':
+        action = 'on'
+    elif get_state == '1':
+        action = 'off'
+
+    toggle_url = "http://" + get_ip + ":5000/node/light?id=led0&action=" + action
+
+    toggle = requests.get(toggle_url)
 
     return "<h1>TEST<h1>"
 
