45c8cbbec3793133b639ee77e4579a08c0a8c42c
kodemeex <115670027+kodemeex@users.noreply.github.com>
2023-12-28 17:26:46 +0000

    check the triggers

--- server_main.py
+++ server_main.py
@@ -39,7 +39,7 @@ def quest(event):
     # if true, trueaction, if false false action
 
     for rule in rules["rule"]:
-        print("Rule: " + str(rule))
+        print(rule["trigger"])
         
                 
 
