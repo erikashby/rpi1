bc85a121f69190175248e8ae5de3ab6a0a291c9e
kodemeex <115670027+kodemeex@users.noreply.github.com>
2023-12-28 18:04:09 +0000

    testing

--- server_main.py
+++ server_main.py
@@ -32,7 +32,7 @@ def quest(event):
     eventtype = event['event']['type']
     eventId = event['event']['ID']
     eventevent = event['event']['event']
-    eventlightstatus = event['status']['light_status'][0]
+    eventlightstatus = event['status']['lightstatus'][0]
     action = None
     
     print(eventlightstatus)
