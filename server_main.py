eb2ac18f7588042a72b686ac3c924f7335c9ed4d
kodemeex <115670027+kodemeex@users.noreply.github.com>
2023-12-29 00:21:55 +0000

    Commented and formatted.

--- server_main.py
+++ server_main.py
@@ -3,31 +3,15 @@ from datetime import datetime
 from flask import Flask, request
 app = Flask(__name__)
 
+# Rules to the quest
 get_rules = open("rules.json")
 rules = json.load(get_rules)
 get_rules.close()
 
+# Temporarily hard code node url
 zero1url = "http://192.168.1.204:5000"
 zero2url = "http://192.168.1.170:5000"
 
-'''
-'name' = 'nodename' << required >>
-'type' = 'node' << required: type of device sending event >>
-'version' = '0.1' << optional: version of aoftware sending event >>
-'source ip' = '192.168.1.204' << optional >>
-'event' = {
-    'datetime' = '(datetime)'   << Optiona: The date/time the event was sent.  If no datetime is included the current server datetme will be assume as the event datetime >>
-    'type' = 'button'  << Required: Type of event 'button', 'triger' etc.>>
-    'ID' = 'id'  << Optional: ID associated with the event >>
-    'event' = '(event name)'  << Required: One of the supported events for the specific type >>
-    'meta' = {(additional meta data)} << Optional: Additional meta data in json format >>
-}
-status =  << Optional status when event is sent from a node>> 
-{  
-    'datetime' = 'datetime',
-    'light_status' : [ {'id'= 'led0','state'=0 },{'id'= 'led1','state'=0 }...]
-}
-'''
 
 def quest(event):
     print(str(event))
@@ -40,11 +24,16 @@ def quest(event):
     # if event match trigger, loop through condition
     # if true, trueaction, if false false action
 
+    # Loop through all the quest in rules
     for rule in rules["rule"]:
         print("Name: " + str(rule["name"]) + "\n\n")
+        # if matches any trigger in the quest, look through the conditions.
         if rule["trigger"]["type"] == eventtype and rule["trigger"]["event"] == eventevent:
             print("triggered!!!\n")
+            # check all the conditions, in condition.
             for c in rule["conditions"]:
+                # if returns true for the condition,
+                # trueaction set, else falseaction set.
                 if check_cond(c):
                     action = rule["trueactions"]
                     print("ACTION TRUE\n")
@@ -53,31 +42,43 @@ def quest(event):
                     action = rule["falseactions"]
                     print("ACTION FALSE\n")
                     break
+    # trigger action based on condition is found.
     trigger_action(action)
         
 
 def trigger_action(action):
+    ### Check type and trigger action, this assumes
+    ### its always a 'REST' type
     for a in action:
         action_url = "http://" + str(a["URL"]) 
         r = requests.get(action_url, str(a["parameter"]))
 
 
 def check_cond(cond):
+    # passes the (rules -> conditions) into cond,
+    # gets condition type
     print("\n Cond: \n" + str(cond) + "\n\n")
     cond_type = cond["type"]
     node_status = ""
     action_output = False
 
-    # figure out what type of condition it is and data.
+    # figure out what type of condition it is and node_status.
+    ### more cases to be added if other types are introduce.
     match cond_type:
         case "nodestatus":
             node_status = get_status_on_node(cond["node"])
 
+    # loop through all condition in conditions (rules -> conditions -> condition)
     for c in cond:
+        # loop all of keys in node_status
         for ns in node_status:
-            # check each of the node status items and look against it.
+            # condition key matches one of node_status keys
             if list(cond["condition"].keys())[0] == ns:
+                # loop through all the items in node_status
+                # one iteration if its a single item.
                 for s in node_status[ns]:
+                    # once it matches the value to the key, set to true
+                    # else it skips, keeps action_output false.
                     if cond["condition"][ns] == s:
                         action_output = True
 
@@ -86,6 +87,9 @@ def check_cond(cond):
 def get_status_on_node(node):
     nodeurl = ""
 
+    ### Hard code for now.
+    ### Database should grab the info
+    ### based on the node is passed.
     if node == "zero1":
         nodeurl = zero1url
     elif node == "zero2":
@@ -93,6 +97,7 @@ def get_status_on_node(node):
     else:
         return None
     
+    # get node status, and return node_status["status"].
     status = requests.get(nodeurl + "/node/status").json()
     print("STATUS:")
     print(status["status"])
@@ -100,76 +105,14 @@ def get_status_on_node(node):
 
 @app.route('/')
 def hello_world():
-    return 'Hello, users!'
+    # maybe have this return server status??? not sure.
+    # or all node names that are in the server right now.
+    return 'Hello, user! This is server.'
 
 @app.route('/event', methods=['PUT'])
 def event():
+    # gets the json sent to server.
+    # passes it to quest
     response = request.json
     quest(response)
     return "200"
-
-@app.route('/testput', methods=["PUT"])
-def test_put():
-    get_ip = '1'
-    get_state = '0'
-    action = 'on'
-    event = request.json
-    for i in event:
-        if i == 'source ip':
-            get_ip = event[i]
-    
-    status_url = "http://" + get_ip + ":5000/node/status"
-    status = requests.get(status_url).json()
-
-    get_state = status['status']['light_status'][0]['state']
-
-    if get_state == 0:
-        action = 'on'
-    elif get_state == 1:
-        action = 'off'
-
-    toggle_url = "http://" + get_ip + ":5000/node/light?id=led0&action=" + action
-
-    toggle = requests.get(toggle_url)
-
-    return "<h1>TEST<h1>"
-
-
-@app.route('/testprint', methods=["PUT"])
-def test_print():
-    response = request.json
-    print(response)
-
-    '''
-    'name' = 'nodename' << required >>
-    'type' = 'node' << required: type of device sending event >>
-    'version' = '0.1' << optional: version of aoftware sending event >>
-    'source ip' = '192.168.1.204' << optional >>
-    'event' = {
-        'datetime' = '(datetime)'   << Optiona: The date/time the event was sent.  If no datetime is included the current server datetme will be assume as the event datetime >>
-        'type' = 'button'  << Required: Type of event 'button', 'triger' etc.>>
-        'ID' = 'id'  << Optional: ID associated with the event >>
-        'event' = '(event name)'  << Required: One of the supported events for the specific type >>
-        'meta' = {(additional meta data)} << Optional: Additional meta data in json format >>
-    }
-    status =  << Optional status when event is sent from a node>> 
-    {  
-        'datetime' = 'datetime',
-        'light_status' : [ {'id'= 'led0','state'=0 },{'id'= 'led1','state'=0 }...]
-    }
-    '''
-
-    print('my name is ' + response['name'])
-    print('my type is ' + response['type'])
-    print('on version v' + response['version'])
-    print('my ip is ' + response['source ip'])
-    print('current time in "event": ' + response['event']['datetime'])
-    print('my event type is ' + response['event']['type'])
-    print('my event ID is ' + response['event']['ID'])
-    print('the ' + response['event']['event'] + ' has been made.')
-    print('my status datetime is ' + response['status']['datetime'])
-    for i in response['status']['light_status']:
-        print('id: ' + i['id'] + ' with state ' + str(i['state']))
-
-    
-    return "<h1>test<h1>"
