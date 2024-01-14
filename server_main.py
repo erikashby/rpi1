192c4e8143b2dbcb154bd258ec4527b186210143
Robert Sabo <kaqemeex@protonmail.com>
2024-01-13 23:13:09 -0700

    test search

--- server_main.py
+++ server_main.py
@@ -39,15 +39,30 @@ def find_nodes():
     # now search for IPs
     for x in range(1,256):
         search_ip = base_ip + str(x)
+        if x < 10 or x > 250:
+            print(search_ip)
         
+find_nodes()
 
-
-
+event_example = '''
+{
+    # the underscore are visuals for optional but not the real name. e.g. _ID = ID
+    'name' = 'name e.g. pico1' << required >>
+    'type' = 'node/REST' << required: type of device sending event >>
+    'event' = '(event name)'  << Required: One of the supported events for the specific type >>
+    'event type' = 'button'  << Required: Type of event 'button', 'triger' etc.>>
+    '_version' = '0.1' << optional: version of aoftware sending event >>
+    '_source ip' = '192.168.1.204' << optional >>
+    '_ID' = 'id'  << Optional: ID associated with the event >>
+    '_meta' = {(additional meta data)} << Optional: Additional meta data in json format >>
+    }
+}
+'''
 # main function for the quest
 def quest(event):
     print(str(event))
-    eventtype = event['event']['type']
-    eventevent = event['event']['event']
+    eventtype = event['event type']
+    eventevent = event['event']
     action = None
 
     # Loop rule array
@@ -78,13 +93,21 @@ def quest(event):
         
 
 def trigger_action(action):
-    ### Check type and trigger action, this assumes
-    ### its always a 'REST' type
+    if action == None:
+        return
+
     for a in action:
         action_url = "http://" + str(a["URL"]) 
         r = requests.get(action_url, str(a["parameter"]))
 
+def check_cond(cond):
+    # passes the (rules -> conditions) into cond,
+    # gets condition type
+    print("\n Cond: \n" + str(cond) + "\n\n")
+    cond_name = cond["actions"]["name"]
+    cond_action = cond["actions"]["action"]
 
+old_check_cond='''
 def check_cond(cond):
     # passes the (rules -> conditions) into cond,
     # gets condition type
@@ -113,28 +136,17 @@ def check_cond(cond):
                     if cond["condition"][ns] == s:
                         action_output = True
 
-    return action_output
+    return action_output'''
 
-def get_status_on_node(node):
-    nodeurl = ""
-
-    ### Hard code for now.
-    ### Database should grab the info
-    ### based on the node is passed.
-    if node == "zero1":
-        nodeurl = zero1url
-    elif node == "zero2":
-        nodeurl = zero2url
-    elif node == "pico1":
-        nodeurl = pico1url
-    else:
-        return None
-    
-    # get node status, and return node_status["status"].
-    status = requests.get(nodeurl + "/status").json()
-    print("STATUS:")
-    print(status["status"])
-    return status["status"]
+def get_status_on_node(nodeurl):
+    status_check = requests.get(nodeurl + "/status")
+    if status_check.status_code == 200:
+        # get node status, and return node_status["status"].
+        status = requests.get(nodeurl + "/status").json()
+        print("STATUS:")
+        print(status)
+        return status
+    return False
 
 @app.route('/')
 def hello_world():
@@ -149,3 +161,33 @@ def event():
     response = request.json
     quest(response)
     return "200"
+
+event_example = '''
+{
+    # the underscore are visuals for optional but not the real name. e.g. _ID = ID
+    'name' = 'name e.g. pico1' << required >>
+    'type' = 'node/REST' << required: type of device sending event >>
+    'event' = '(event name)'  << Required: One of the supported events for the specific type >>
+    'event type' = 'button'  << Required: Type of event 'button', 'triger' etc.>>
+    '_version' = '0.1' << optional: version of aoftware sending event >>
+    '_source ip' = '192.168.1.204' << optional >>
+    '_ID' = 'id'  << Optional: ID associated with the event >>
+    '_meta' = {(additional meta data)} << Optional: Additional meta data in json format >>
+    }
+}
+'''
+
+@app.route('/fireon')
+def fireon():
+    # turn fire one
+
+    response = {"name":"URL", "type":"REST", "event":"API"}
+    quest(response)
+    return "fire on"
+
+@app.route('/fireoff')
+def fireoff():
+    # turn fire off
+    response = request.json
+    quest(response)
+    return "fire off"
